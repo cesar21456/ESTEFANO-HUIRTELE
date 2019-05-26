1993ee40c795e6f9c6464a8a55b0a2f6985a6c1e
Stephano <stephano.wurttele@utec.edu.pe>
2019-05-26 15:37:21 -0500

    Trabajo de primera semana

--- tb.v
+++ tb.v
@@ -1,15 +1,26 @@
 module tb();
-reg dir;
+reg clk,pc;
+wire dir,direccion,RegDst,Branch,MemRead,MemtoReg,ALUOP,Memwrite,ALUSrc,RegWrite,Jump;
 wire[31:0] instruction;
-
+reg[7:0] memory[0:255];
+PC programcounter(clk,pc,direccion,dir);
 InstructionMemory test1(dir,instruction);
+ControlUNIT controlunit(instruction[31:26],RegDst,Branch,MemRead,MemtoReg,ALUOP,Memwrite,ALUSrc,RegWrite,Jump);
 
 initial
 begin
-	dir=8'h00;
+	pc=0;
+	clk=1;
+	clk=~clk;
+	#10;
+	pc=4;
+	clk=~clk;
+	clk=~clk;
+
 end
 initial
 begin
-	$monitor("Instruction is %b",instruction);end
+	$monitor("Instruction is %b, Regwrite is %b, AluOP is %b",instruction,RegWrite,ALUOP);
+end
 endmodule
 
