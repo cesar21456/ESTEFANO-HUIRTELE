627052c3080d170baf0d57581ecb53a7ea47e1ee
cesar21456 <39863125+cesar21456@users.noreply.github.com>
2019-06-01 22:22:52 -0500

    Add files via upload

--- tb.v
+++ tb.v
@@ -1,35 +1,31 @@
+
 module tb();
 reg clk;
-wire pc1;
-wire[31:0] direccion,dir,pc;
+reg [31:0] pc;
 wire RegDst,Branch,MemRead,MemtoReg,ALUOP,Memwrite,ALUSrc,RegWrite,Jump;
-wire[31:0] instruction;
-reg[7:0] memory[0:255];
-reg cont;
-PC programcounter(clk,pc,direccion,pc1);
-PCAdder add(direccion,pc);
-InstructionMemory test1(direccion,instruction);
+wire[31:0] instruction,dir;
+wire [31:0] read1,read2,salida_alu;
+wire[31:0] salida_signextend,salida_mux;
+wire overflow;
+PC programcounter(clk,pc,dir);
+InstructionMemory test1(dir,instruction);
 ControlUNIT controlunit(instruction[31:26],RegDst,Branch,MemRead,MemtoReg,ALUOP,Memwrite,ALUSrc,RegWrite,Jump);
+RegFile regfile(RegWrite,clk,instruction[25:21],instruction[20:16],instruction[15:11],read1,read2,salida_alu);
+signextension SIGNEXTENSION(instruction[15:0],salida_signextend);
+mux MUX(read2,salida_signextend,ALUSrc,salida_mux);
+alu ALU(read1,salida_mux,overflow,instruction[5:0],salida_alu);
+
+
 
 initial
-
 begin
+	pc=0;
 	clk=1;
-	clk=~clk;
-	cont=0;
-	#10;
-	clk=1;
-	clk=~clk;
-	cont=1;
-	#10;
-	clk=1;
-	clk=~clk;
-	cont=0;
+	
 
 end
 initial
 begin
-	$monitor("Instruction is %b, Regwrite is %b, AluOP is %b, direccion es %b,contador es %b",instruction,RegWrite,ALUOP,direccion,cont);
+	$monitor("Instruction is %b, Regwrite is %b, AluOP is %b",instruction,RegWrite,ALUOP);
 end
 endmodule
-
