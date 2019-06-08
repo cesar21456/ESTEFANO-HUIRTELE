1377f1db15b1332e665abde6153001c702bc5a9f
cesar21456 <39863125+cesar21456@users.noreply.github.com>
2019-06-08 18:17:01 -0500

    Update Regfile.v

--- Regfile.v
+++ Regfile.v
@@ -8,15 +8,18 @@ output [31:0] read1,read2;
 initial
 begin
 	$readmemb("text.txt",memory);
+	
 end
-        assign read1=(RegWrite)?memory[inA]:0;
-        assign read2=(RegWrite)?memory[inB]:0;
+        assign read1=memory[inA];
+        assign read2=memory[inB];
+
 	
 always@(negedge clk)
 begin
-	if(RegWrite)begin
+	if(RegWrite==1)begin
         memory[inC]=out;
 	$display("guardado en posicion %b la cadena %b (prueba: %b)", inC, out, memory[inC]);
 end
 end
 endmodule
+
