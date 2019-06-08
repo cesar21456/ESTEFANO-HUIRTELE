78afeb73f24ba35e0cdf17eb730dab17b457ac64
cesar21456 <39863125+cesar21456@users.noreply.github.com>
2019-06-08 18:15:46 -0500

    Update ControlUNIT.v

--- ControlUNIT.v
+++ ControlUNIT.v
@@ -1,26 +1,31 @@
-module ControlUNIT(input [5:0] primeros_seis,output RegDst,Branch,MemRead,MemtoReg,ALUOP,Memwrite,ALUSrc,RegWrite,Jump);
+module ControlUNIT(input [5:0] primeros_seis,output RegDst,Branch,MemRead,MemtoReg,Memwrite,ALUSrc,RegWrite,Jump,output wire[6:0] ALUOP);
 
 assign RegWrite= (primeros_seis==0)?1: //Instrucciones tipo R
-	                 ((primeros_seis>=8)&(primeros_seis<=14))?1:0; //Inmediatos
+		 ((primeros_seis>=8)&(primeros_seis<=13)&(primeros_seis!=9)&(primeros_seis!=11))?1: //Inmediatos
+		(primeros_seis==32|primeros_seis==33|primeros_seis==35)?1:0; //load word
 
 
 assign RegDst= (primeros_seis==0)?1:0;//Instrucciones tipo R
 
-assign ALUOP= (primeros_seis==0)?1: //Instrucciones tipo R
-                ((primeros_seis>=8)&(primeros_seis<=14))?1: //Inmediatos
-                ((primeros_seis==1)|(primeros_seis>=4)&(primeros_seis<=7))?1:0; //Branches y Jumps
 
-assign ALUSrc= ((primeros_seis>=8)&(primeros_seis<=14))?1:0;//Inmediatos
+assign ALUOP= ((primeros_seis==1)|(primeros_seis==4)|(primeros_seis==5))?{1'b1,primeros_seis}: //Branches 
+	       ((primeros_seis>=8)&(primeros_seis<=13)&(primeros_seis!=9)&(primeros_seis!=11))?{1'b1,primeros_seis}: //Inmediatos
+		(primeros_seis==40|primeros_seis==41|primeros_seis==43)?{1'b1,primeros_seis}: //store word
+		(primeros_seis==32|primeros_seis==33|primeros_seis==35)?{1'b1,primeros_seis}: //load word
+		(primeros_seis==0)?7'b0000000:0; //Instrucciones tipo R
 
-assign Branch= ((primeros_seis==1)|(primeros_seis>=4)&(primeros_seis<=7))?1:0; //Branches
+assign ALUSrc= ((primeros_seis>=8)&(primeros_seis<=13)&(primeros_seis!=9)&(primeros_seis!=11))?1:
+		(primeros_seis==40|primeros_seis==41|primeros_seis==43)?1: //store word
+		(primeros_seis==32|primeros_seis==33|primeros_seis==35)?1:0; //load word//Inmediatos
 
-assign Jump= (primeros_seis==1|primeros_seis==2)?1:0; //Jump
+assign Branch= ((primeros_seis==1)|(primeros_seis==4)|(primeros_seis==5))?1:0; //Branches
 
-assign MemRead=(primeros_seis>=32 & primeros_seis<=38)?1:0; //Load word
+assign Jump= (primeros_seis==2|primeros_seis==3|primeros_seis==0)?1:0; //Jump
 
-assign MemWrite=(primeros_seis>=40 & primeros_seis<=46)?1:0; //Store word
+assign MemRead=(primeros_seis==32|primeros_seis==33|primeros_seis==35)?1:0; //Load word
+
+assign MemWrite=(primeros_seis==40|primeros_seis==41|primeros_seis==43)?1:0; //Store word
 
 assign MemtoReg=(primeros_seis>=32 & primeros_seis<=38)?1:0; //Load word
 
-endmodule
-						
+endmodule 
