1bf858518cece161220d755ac4bd49c1a8323e37
cesar21456 <39863125+cesar21456@users.noreply.github.com>
2019-06-08 14:11:59 -0500

    Update tb.v

--- tb.v
+++ tb.v
@@ -1,4 +1,5 @@
 
+
 module tb();
 reg clk;
 reg [31:0] pc;
@@ -7,45 +8,28 @@ wire[31:0] instruction,dir;
 wire [31:0] read1,read2,salida_alu;
 wire[31:0] salida_signextend,salida_mux;
 wire overflow;
+wire READDATA;
+wire salida_mux_2;
 PC programcounter(clk,pc,dir);
 InstructionMemory test1(dir,instruction);
 ControlUNIT controlunit(instruction[31:26],RegDst,Branch,MemRead,MemtoReg,ALUOP,Memwrite,ALUSrc,RegWrite,Jump);
-RegFile regfile(RegWrite,clk,instruction[25:21],instruction[20:16],instruction[15:11],read1,read2,salida_alu);
+RegFile regfile(RegWrite,clk,instruction[25:21],instruction[20:16],instruction[15:11],read1,read2,salida_mux_2);
 signextension SIGNEXTENSION(instruction[15:0],salida_signextend);
-mux MUX(read2,salida_signextend,ALUSrc,salida_mux);
+mux MUX_REGISTER_MEMORY(salida_signextend,read2,ALUSrc,salida_mux);
 alu ALU(read1,salida_mux,overflow,instruction[5:0],salida_alu);
-
+datamemory DATAMEMORY(Memwrite,MemRead,salida_alu,read2,READDATA);
+mux MUX_DATA_MEMORY(READDATA,salida_alu,MemtoReg,salida_mux_2);
 
 
 initial
 begin
-        clk=0;
-        cont=0;
-        #10;
-        clk=1;
-        cont=1;
-        clk=0;
-        #10;
-        clk=1;
-        cont+=1;
-        clk=0;
+	pc=0;
+	clk=1;
+	
+
 end
 initial
 begin
 	$monitor("Instruction is %b, Regwrite is %b, AluOP is %b",instruction,RegWrite,ALUOP);
 end
-	
-
-/*
-always
-begin
-      #10
-      clk <= 1;
-      #5
-      clk <= 0;
-end
-*/
-
-	
-	
 endmodule
