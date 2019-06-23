fdaba1c4e74bc8709e552bb4ac5e6df1f20ee440
Stephano <stephano.wurttele@utec.edu.pe>
2019-06-22 20:11:43 -0500

    loads y saves

--- ControlUNIT.v
+++ ControlUNIT.v
@@ -1,4 +1,4 @@
-module ControlUNIT(input [5:0] primeros_seis,output RegDst,Branch,MemRead,MemtoReg,Memwrite,ALUSrc,RegWrite,Jump,output wire[6:0] ALUOP);
+module ControlUNIT(input [5:0] primeros_seis,output RegDst,Branch,MemtoReg,Memwrite,ALUSrc,RegWrite,Jump,output wire[6:0] ALUOP,output[1:0] MemRead);
 
 assign RegWrite= (primeros_seis==0)?1: //Instrucciones tipo R
 		 ((primeros_seis>=8)&(primeros_seis<=13)&(primeros_seis!=9)&(primeros_seis!=11))?1: //Inmediatos
@@ -15,16 +15,16 @@ assign ALUOP= ((primeros_seis==1)|(primeros_seis==4)|(primeros_seis==5))?{1'b1,p
 		(primeros_seis==0)?7'b0000000:0; //Instrucciones tipo R
 
 assign ALUSrc= ((primeros_seis>=8)&(primeros_seis<=13)&(primeros_seis!=9)&(primeros_seis!=11))?1:
-		(primeros_seis==40|primeros_seis==41|primeros_seis==43)?1: //store word
-		(primeros_seis==32|primeros_seis==33|primeros_seis==35)?1:0; //load word//Inmediatos
+		(primeros_seis==40|primeros_seis==41|primeros_seis==43)?1: //store
+		(primeros_seis==32|primeros_seis==33|primeros_seis==35)?1:0; //load//Inmediatos
 
 assign Branch= ((primeros_seis==1)|(primeros_seis==4)|(primeros_seis==5))?1:0; //Branches
 
 assign Jump= (primeros_seis==2|primeros_seis==3|primeros_seis==0)?1:0; //Jump
 
-assign MemRead=(primeros_seis==32|primeros_seis==33|primeros_seis==35)?1:0; //Load word
+assign MemRead=(primeros_seis==32)?(2'b01):(primeros_seis==33)?(2'b10):(primeros_seis==35)?(2'b11):0; //Load
 
-assign MemWrite=(primeros_seis==40|primeros_seis==41|primeros_seis==43)?1:0; //Store word
+assign MemWrite=(primeros_seis==40|primeros_seis==41|primeros_seis==43)?1:0; //Store
 
 assign MemtoReg=(primeros_seis>=32 & primeros_seis<=38)?1:0; //Load word
 
--- tb.v
+++ tb.v
@@ -1,7 +1,7 @@
-
 module tb();
 reg clk;
-wire RegDst,Branch,MemRead,MemtoReg,Memwrite,ALUSrc,RegWrite,Jump;
+wire RegDst,Branch,MemtoReg,Memwrite,ALUSrc,RegWrite,Jump;
+wire [1:0]MemRead;
 wire [6:0]ALUOP;
 wire [5:0] control;
 wire[31:0] instruction,dir;
@@ -10,16 +10,19 @@ wire zero;
 wire[31:0] salida_signextend,salida_mux;
 wire overflow;
 wire [31:0]READDATA;
-wire [31:0]salida_mux_2;
+wire [31:0]salida_mux_2,salida_mux_1;
+
 PC programcounter(clk,dir);
 InstructionMemory test1(dir,instruction);
-ControlUNIT controlunit(instruction[31:26],RegDst,Branch,MemRead,MemtoReg,Memwrite,ALUSrc,RegWrite,Jump, ALUOP);
+ControlUNIT controlunit(instruction[31:26],RegDst,Branch,MemtoReg,Memwrite,ALUSrc,RegWrite,Jump, ALUOP,MemRead);
+mux MUX_JUMP(1,0,Jump,salida_mux_1);
+
 RegFile regfile(RegWrite,clk,instruction[25:21],instruction[20:16],instruction[15:11],read1,read2,salida_mux_2);
 signextension SIGNEXTENSION(instruction[15:0],salida_signextend);
 mux MUX_REGISTER_MEMORY(salida_signextend,read2,ALUSrc,salida_mux);
 alucontrol ALUCONTROL(instruction[5:0],ALUOP,control);
 alu ALU(read1,salida_mux,overflow,control,salida_alu,zero);
-datamemory DATAMEMORY(Memwrite,MemRead,salida_alu,read2,READDATA);
+datamemory DATAMEMORY(Memwrite,salida_alu,read2,READDATA,MemRead);
 mux MUX_DATA_MEMORY(READDATA,salida_alu,MemtoReg,salida_mux_2);
 
 
