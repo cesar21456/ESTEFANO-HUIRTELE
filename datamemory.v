65d96ac85b0d112279e879d7a58882c72ee7202a
Stephano <stephano.wurttele@utec.edu.pe>
2019-06-22 20:32:08 -0500

    Regfile uploaded

--- datamemory.v
+++ datamemory.v
@@ -1,17 +1,19 @@
 
-module datamemory(input MemWrite, MemRead,input[31:0] Address, WriteData,output wire[31:0]ReadData);
+module datamemory(input MemWrite,input[31:0] Address, WriteData,output wire[31:0]ReadData,output wire[1:0] MemRead);
 reg [31:0]DATA[0:63];
 
 initial begin
 	$readmemb("memory.txt",DATA);
 end
 
-assign ReadData=(MemRead)?DATA[Address]:0;
+assign ReadData=(MemRead==1)?{24'b0,DATA[Address][7:0]}:
+		(MemRead==2)?{16'b0,DATA[Address][15:0]}:
+		(MemRead==3)?(DATA[Address]):0;
 
 always@(*)begin
 if (MemWrite)
 begin
-	DATA[Address]=WriteData;
+	DATA[Address]={16'b0,WriteData[15:0]};
 	$display("la data escrita en %b es WriteData %b y lo que se guardo fue %b",Address,WriteData,DATA[Address]);
 end
 end
--- signextension.v
+++ signextension.v
@@ -2,7 +2,7 @@ module signextension(inD,inD2);
 input[15:0] inD;
 output [31:0] inD2;
 
-	assign inD2[31:16]=0;
+	assign inD2[31:16]=inD[15];
 	assign inD2[15:0]=inD;
 
 endmodule
