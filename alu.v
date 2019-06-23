7b6fac70e6a01e40c6839f899a63059f11f8e7f8
cesar21456 <39863125+cesar21456@users.noreply.github.com>
2019-06-22 22:29:09 -0500

    Update alu.v

--- alu.v
+++ alu.v
@@ -1,10 +1,9 @@
-module alu(read1,foutput,overflow,control,out,zero);
+endmodulemodule alu(read1,foutput,overflow,control,out,zero);
 output reg overflow;
-reg [31:0] oc;
-output wire[31:0]out;
+reg signed[31:0] oc ;
+output wire [31:0]out ;
 input[5:0]control;
-input[31:0] foutput,read1;
-reg[31:0] memory[31:0];
+input signed[31:0] foutput,read1 ;
 output wire zero;
 
 always @(*)
@@ -16,12 +15,12 @@ begin
 	end
 end
 
-assign out=(control==36)?read1&foutput:(
-	   	(control==35)?read1|foutput:(
-			(control==16)?oc:(
+assign out=(control==36|control==12)?read1&foutput:(
+	   	(control==35|control==13)?read1|foutput:(
+			(control==16|control==8)?oc:(
 	   			(control==34)?read1-foutput:(
-	   				(control==42)?((read1<foutput)?1:0):     
-					(control==39)?~(read1|foutput):1   ))));
+	   				(control==42|control==10)?((read1<foutput)?1:0):     
+					(control==39)?~(read1|foutput):0   ))));
 
 assign zero=(control==4&((read1-foutput)==0))?1:(
 	     (control==5&(read1-foutput)!=0)?1:0);
