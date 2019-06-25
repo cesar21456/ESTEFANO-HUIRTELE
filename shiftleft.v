1840a6b88518682fcd8725349760655760fabf06
StephanoWurttele <39769681+StephanoWurttele@users.noreply.github.com>
2019-06-25 16:55:55 -0500

    Add files via upload

---
+++ shiftleft.v
@@ -0,0 +1,10 @@
+module shiftleft(jumpi,fjump);
+input[25:0] jumpi;
+output[27:0] fjump;
+assign fjump=jumpi*4;
+
+always@(*)
+begin
+	$display("jumpi is %b fjump is %b",jumpi,fjump);
+end
+endmodule
